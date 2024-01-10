42f7fe92dec36aa28f7f25518a42350f420e4b6f
Hanzalashaik <shaikhhanzala27@gmail.com>
2024-01-10 08:42:30 +0000

    Completed✅

---
+++ server/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url); //
+const __dirname = path.dirname(__filename); //
+const app = express();
+
+const port = 5007;
+
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build"));
+});
+
+app.listen(port, (req, res) => {
+  console.log(`Listening on Port Number ${port}`);
+});
